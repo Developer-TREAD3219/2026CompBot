79cd195c6ef055b22fae08a46159f2e99ffef2d7
TREAD 3219 <Tread3219@gmail.com>
2026-01-23 16:22:50 -0800

    kwb: removing some cruft

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,8 +4,6 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.AddressableLED;
-import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -27,7 +25,6 @@ public class Robot extends TimedRobot {
   public Robot() {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
-    m_led.setLength(m_ledBuffer.getLength());
     // Set the data
     m_robotContainer = new RobotContainer();
   }
